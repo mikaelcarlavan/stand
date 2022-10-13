d6304ea5461b1aad2b000ff178f1e2aa27e44736
Mikael Carlavan <contact@mika-carl.fr>
2022-10-13 16:12:03 +0200

    Correction requête

--- class/stand.class.php
+++ class/stand.class.php
@@ -491,7 +491,7 @@ class Stand extends CommonObject
         $sql = 'SELECT l.rowid, l.fk_stand, l.note, l.fk_user, l.user_author_id, l.datec, l.tms ';
         $sql .= ' FROM '.MAIN_DB_PREFIX.'standdet as l';
         $sql .= ' WHERE l.fk_stand = '.$this->id;
-        $sql .= ' ORDER BY l.rowid';
+        $sql .= ' ORDER BY l.rowid DESC';
 
         dol_syslog(get_class($this)."::fetch_lines", LOG_DEBUG);
         $result = $this->db->query($sql);
@@ -882,6 +882,32 @@ class Stand extends CommonObject
         }
     }
 
+    /**
+     *  Load last line id for user
+     *
+     *  @param  int		$fk_user          Id user
+     *  @return    StandLine              <0 if KO, >0 if OK
+     */
+    public function fetch_last_lineid($fk_user = 0)
+    {
+        $sql = 'SELECT cd.rowid';
+        $sql .= ' FROM '.MAIN_DB_PREFIX.'standdet as cd';
+        $sql .= ' WHERE cd.fk_stand = '.$this->id;
+        $sql .= ' AND cd.fk_user = '.((int) $fk_user);
+        $sql .= ' LIMIT 1';
+        $result = $this->db->query($sql);
+        if ($result) {
+            $objp = $this->db->fetch_object($result);
+            if ($objp) {
+                return $objp->rowid;
+            } else {
+                return 0;
+            }
+        } else {
+            $this->error = $this->db->lasterror();
+            return -1;
+        }
+    }
 
     /**
      *    Add a stand line into database (linked to product/service or not)
